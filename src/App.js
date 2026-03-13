96fcd106f97f6c2b08c3459249b6c62c0ba24c66
rendezvxus <realsuppaplex@gmail.com>
2026-03-13 20:10:36 +0300

    Updated file structure, added HTML basic markup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+class App {
+    constructor(header, main, footer) {
+        this.header = header
+        this.main = main
+        this.footer = footer
+    }
+
+    init() {
+        
+    }
+}
