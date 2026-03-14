ca0f78ec032bd2913442715bacb5a1b3ce634521
rendezvxus <realsuppaplex@gmail.com>
2026-03-14 19:52:43 +0300

    added base url in vite cfg

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import path from "path"
 export default defineConfig({
+    base: "/vistegra-shop",
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
