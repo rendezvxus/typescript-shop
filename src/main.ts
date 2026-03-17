3ce527326543e1957bb13f10060bf54b5103cfad
rendezvxus <realsuppaplex@gmail.com>
2026-03-17 16:08:23 +0300

    main.js -> main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import './style.scss'
+import App from '@/App.js'
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+        const header = document.querySelector('#header')
+        const main = document.querySelector('#main')
+        const footer = document.querySelector('#footer')
 
-})
+        
+        const app = new App(header, main, footer)
+
+        app.init()
+    }
+)
